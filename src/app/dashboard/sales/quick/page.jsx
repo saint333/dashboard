3443d2470e5e0987250754ff3b4689c8e1c4c02b
David Israel Granados Elias <israeldavi0904@gmail.com>
2024-06-03 01:01:16 -0500

    ADD: haciendo prueba de autocompletado

--- src/app/dashboard/sales/quick/page.jsx
+++ src/app/dashboard/sales/quick/page.jsx
@@ -1,68 +1,108 @@
-"use client";
-import Table from "@/components/table";
-import {
-  Autocomplete,
-  Button,
-  Card,
-  CardContent,
-  Divider,
-  MenuItem,
-  TextField,
-} from "@mui/material";
-import { useMemo } from "react";
-import { useForm } from "react-hook-form";
-import SaveIcon from "@mui/icons-material/Save";
+// "use client";
+// import Table from "@/components/table";
+// import {
+//   Autocomplete,
+//   Button,
+//   Card,
+//   CardContent,
+//   Divider,
+//   MenuItem,
+//   TextField,
+// } from "@mui/material";
+// import { useEffect, useMemo, useState } from "react";
+// import { useForm } from "react-hook-form";
+// import SaveIcon from "@mui/icons-material/Save";
 
-export default function Quick() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+import AutocompleteCustom from "@/components/autocomplete";
+  const data = async () => {
+      const responde = await fetch(
+        "https://b1rf2oxmn2.execute-api.us-east-2.amazonaws.com/maintenance/product",
+        {
+          method: "GET",
+          headers: {
+            Authorization:
+              "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RpZ28iOjAsIm1lbnNhamUiOiJMb2dpbiBleGl0b3NvIiwicF9pbmlkdXN1YXJpbyI6MSwiY2hub21icmVzIjoiU0FOQ0hFWiBBUklBUywgSk9TRSBNQVgiLCJwX2luaWRkb21pbmlvX2RlZmF1bHQiOjEsInBfaW5pZHN1Y3Vyc2FsX2RlZmF1bHQiOjEsInBfaW5pZHBlcmZpbCI6MSwiaWF0IjoxNzE3MzkyODkxLCJleHAiOjE3MTczOTY0OTF9.2PU97_CKu93TuSfL4fO9ELSSFH_PlelzGobChFSj24E",
+          },
+        }
+      );
+      const lists = await responde.json();
+      console.log("peticion");
+      return lists.map((e, i) => e.chdescripcion + i)
+    };
 
-  const onSubmit = (data) => {
-    console.log("🚀 ~ onSubmit ~ data:", data);
-  };
+export default async function Quick() {
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm();
 
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: "name.firstName", //access nested data with dot notation
-        header: "First Name",
-        size: 150,
-      },
-      {
-        accessorKey: "name.lastName",
-        header: "Last Name",
-        size: 150,
-      },
-      {
-        accessorKey: "address", //normal accessorKey
-        header: "Address",
-        size: 200,
-      },
-      {
-        accessorKey: "city",
-        header: "City",
-        size: 150,
-      },
-      {
-        accessorKey: "state",
-        header: "State",
-        size: 150,
-      },
-    ],
-    []
-  );
+  // const [list, setList] = useState([]);
+
+  // const onSubmit = (data) => {
+  //   console.log("🚀 ~ onSubmit ~ data:", data);
+  // };
+
+  // const columns = useMemo(
+  //   () => [
+  //     {
+  //       accessorKey: "name.firstName", //access nested data with dot notation
+  //       header: "First Name",
+  //       size: 150,
+  //     },
+  //     {
+  //       accessorKey: "name.lastName",
+  //       header: "Last Name",
+  //       size: 150,
+  //     },
+  //     {
+  //       accessorKey: "address", //normal accessorKey
+  //       header: "Address",
+  //       size: 200,
+  //     },
+  //     {
+  //       accessorKey: "city",
+  //       header: "City",
+  //       size: 150,
+  //     },
+  //     {
+  //       accessorKey: "state",
+  //       header: "State",
+  //       size: 150,
+  //     },
+  //   ],
+  //   []
+  // );
+
+  // useEffect(() => {
+  //   const data = async () => {
+  //     const responde = await fetch(
+  //       "https://b1rf2oxmn2.execute-api.us-east-2.amazonaws.com/maintenance/product",
+  //       {
+  //         method: "GET",
+  //         headers: {
+  //           Authorization:
+  //             "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RpZ28iOjAsIm1lbnNhamUiOiJMb2dpbiBleGl0b3NvIiwicF9pbmlkdXN1YXJpbyI6MSwiY2hub21icmVzIjoiU0FOQ0hFWiBBUklBUywgSk9TRSBNQVgiLCJwX2luaWRkb21pbmlvX2RlZmF1bHQiOjEsInBfaW5pZHN1Y3Vyc2FsX2RlZmF1bHQiOjEsInBfaW5pZHBlcmZpbCI6MSwiaWF0IjoxNzE3MzkyODkxLCJleHAiOjE3MTczOTY0OTF9.2PU97_CKu93TuSfL4fO9ELSSFH_PlelzGobChFSj24E",
+  //         },
+  //       }
+  //     );
+  //     const lists = await responde.json();
+  //     setList(lists.map((e, i) => e.chdescripcion + i));
+  //   };
+  //   data();
+  // }, []);
+
+  const list = await data();
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-[1.5fr_1fr] gap-6 grid-rows-[auto] items-start'>
-      <Card>
+      <AutocompleteCustom data={list}/>
+      {/* <Card>
         <CardContent>
           <Autocomplete
             disablePortal
             id='combo-box-demo'
-            options={["Movie 1", "Movie 2", "Movie 3"]}
+            options={list}
             size='small'
             fullWidth
             noOptionsText='No hay opciones'
@@ -171,13 +211,13 @@ export default function Quick() {
                 noOptionsText='No hay opciones'
                 renderInput={(params) => (
                   <TextField
-                  label='Cliente RUC/DNI/OTROS'
-                  error={errors.client}
-                  helperText={
-                    errors.client ? "Este campo es requerido" : null
-                  }
-                  {...register("client", { required: true })}
-                  {...params}
+                    label='Cliente RUC/DNI/OTROS'
+                    error={errors.client}
+                    helperText={
+                      errors.client ? "Este campo es requerido" : null
+                    }
+                    {...register("client", { required: true })}
+                    {...params}
                   />
                 )}
               />
@@ -271,7 +311,7 @@ export default function Quick() {
             </div>
           </CardContent>
         </Card>
-      </div>
+      </div> */}
     </div>
   );
 }
---
+++ src/components/autocomplete/index.jsx
@@ -0,0 +1,17 @@
+'use client'
+import { Autocomplete, TextField } from '@mui/material'
+import React from 'react'
+
+export default function AutocompleteCustom({data}) {
+  return (
+    <Autocomplete
+            disablePortal
+            id='combo-box-demo'
+            options={data}
+            size='small'
+            fullWidth
+            noOptionsText='No hay opciones'
+            renderInput={(params) => <TextField {...params} label='Movie' />}
+          />
+  )
+}
